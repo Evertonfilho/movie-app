77538cc94e53631d6ba311e36cc80c4bdf951454
evertonfilho <evertonfilho0513@gmail.com>
2024-04-03 23:56:34 -0300

    pages, config mightymeld

---
+++ src/routes/index.tsx
@@ -0,0 +1,29 @@
+import { createBrowserRouter } from "react-router-dom";
+import Home from "../pages/home";
+import Error from "../pages/error";
+import Bookmark from "../pages/bookmark";
+import Movies from "../pages/movies";
+import TvSeries from "../pages/tv-series";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+    errorElement: <Error />,
+  },
+  {
+    path: "/bookmark",
+    element: <Bookmark />,
+    errorElement: <Error />,
+  },
+  {
+    path: "/movies",
+    element: <Movies />,
+    errorElement: <Error />,
+  },
+  {
+    path: "/tv-series",
+    element: <TvSeries />,
+    errorElement: <Error />,
+  },
+]);
